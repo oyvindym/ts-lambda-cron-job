9bab51024621676a51c8cb48987d35aff92c4fa3
Øyvind Moldestad <moldestad@gmail.com>
2019-11-25 22:42:39 +0100

    Update makefiles to include project name

--- function-code/rollup.config.js
+++ function-code/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default {
-  input: `src/${process.env.FUNCTION}.ts`,
+  input: `src/${process.env.FUNCTION_NAME}.ts`,
   output: [
     {
       file: pkg.main,
